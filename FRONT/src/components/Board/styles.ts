40ef2aa8bf4d989408201a993787b8f306dde89f
João Luiz <jleaopitta@gmail.com>
2022-05-13 09:51:40 -0300

    style: responsive grid

--- FRONT/src/components/Board/styles.ts
+++ FRONT/src/components/Board/styles.ts
@@ -4,10 +4,18 @@ export const Wrapper = styled.div`
   padding: 7rem;
   min-height: 100vh;
 
+  @media (max-width: 514px) {
+    padding: 2rem;
+  }
+
   > div {
     display: grid;
     grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
     gap: 1rem;
     padding: 0.625rem;
+
+    @media (max-width: 514px) {
+      grid-template-columns: 1fr;
+    }
   }
 `;
